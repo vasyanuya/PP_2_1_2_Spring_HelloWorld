e68f73cf1acd3bc04b67ba7a4c5f7c407320fe88
Vasiliy Gudkov <vasyanuya@google.com>
2024-03-14 11:12:56 +0300

    1

---
+++ src/main/java/Cat.java
@@ -0,0 +1,11 @@
+public class Cat {
+    private String bark;
+
+    public String getMessage() {
+        return bark;
+    }
+
+    public void setMessage(String bark) {
+        this.bark = bark;
+    }
+}
